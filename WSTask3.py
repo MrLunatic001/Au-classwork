eb57a90a2023821097c69673247360a4c8fd9a7f
Adriel <63085003+MrLunatic001@users.noreply.github.com>
2020-09-09 20:17:33 +0100

    Add files via upload

---
+++ WSTask3.py
@@ -0,0 +1,23 @@
+while True:
+    try:
+        car_mileage_past = float(input("Input the car mileage the last time the car was filed: "))
+        break
+    except:
+        print("Enter a number")
+while True:
+    try:
+        car_mileage_present = float(input("Input the car mileage now"))
+        break
+    except:
+        print("Enter a number")
+while True:
+    try:
+        number_of_liters = float(input("Input the total number of liters taken to fill the tank: "))
+        break
+    except:
+        print("Enter a number: ")
+gallon = number_of_liters * 4.546
+miles_per_gallon = float(car_mileage_past - car_mileage_present) / gallon
+print("The number of miles per gallon for your car is : " + str(round(miles_per_gallon, 2)))
+
+
---
+++ WSTask4.py
@@ -0,0 +1,21 @@
+import math
+while True:
+    try:
+        students = int(input("Input the number of students: "))
+        break
+    except:
+        print("Enter an integer")
+while True:
+    try:
+        books = int(input("Input the number of books: "))
+        break
+    except:
+        print("Enter an integer")
+print("The number of books that each student will receive: " + str(round(books/students)))
+if round(books/ students) == 0:
+    print("The number of books left over: " + str(students))
+else:
+    print("The number of books left over: " + str(books % students))
+
+name = input("Enter a name: ")
+print("The length of the name is: " + str(len(name)))
